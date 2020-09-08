a15c4ce6b4060fb80903a9fce1f7f1a786644d79
UxDeveloper82 <electronicsmf@aol.com>
2020-09-07 22:48:46 -0400

    Creating Panel Controller

---
+++ Controllers/PanelController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartsInventory.Data.Repository;
+using PartsInventory.Models;
+using PartsInventory.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartsInventory.Controllers
+{
+    public class PanelController : Controller
+    {
+        private readonly IPostRepository _repo;
+
+        public PanelController(IPostRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public IActionResult Index() 
+        {
+            var posts = _repo.GetAllPosts();
+            return View(posts);
+        }
+
+        public IActionResult Post(int id)
+        {
+            var post = _repo.GetPost(id);
+            return View(post);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return View(new PostViewModel());
+            }
+            else 
+            {
+                var post = _repo.GetPost((int)id);
+                return View(new PostViewModel
+                {
+                    Id = post.Id,
+                    Title = post.Title,
+                    CreatedBy = post.CreatedBy,
+                    Description = post.Description,
+                    Category = post.Category,
+                });
+           
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(PostViewModel vm)
+        {
+            var post = new Post
+            {
+                Id = vm.Id,
+                Title = vm.Title,
+                CreatedBy = vm.CreatedBy,
+                Description = vm.Description,
+                Category = vm.Category
+            };
+
+
+            if (post.Id > 0)
+                _repo.UpdatePost(post);
+            else
+                _repo.AddPost(post);
+            if (await _repo.SaveChangesAsync())
+                return RedirectToAction("Index");
+            else
+                return View(post);
+        }
+
+
+
+    }
+}
--- ViewModels/PostViewModel.cs
+++ ViewModels/PostViewModel.cs
@@ -4,18 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PartsInventory.Models
+namespace PartsInventory.ViewModels
 {
-    public class Post
+    public class PostViewModel
     {
         public int Id { get; set; }
         public string Title { get; set; } = "";
         public string Body { get; set; } = "";
-        public string Image { get; set; } = "";
         public string Description { get; set; } = "";
         public string CreatedBy { get; set; } = "";
         public string Category { get; set; } = "";
 
-        public DateTime Created { get; set; } = DateTime.Now;
     }
 }
